197bed0053ebc023b69295a02f1359dba9f7cc23
Gray Hunter <mgrayhunter@arizona.edu>
2024-06-25 16:04:22 -0700

    Initial commit

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# Simulate and Plot Data - Learning GitHub ;)
+# M. Gray Hunter
+# mgrayhunter@arizona.edu
+# August 25th, 2024 
+
+# Simulate predictor variable 
+x <- rnorm(n=100)
+# Simulate response variable with some noise
+y <- 2*x + rnorm(n=100,sd=0.2)
+# Plot the data
+plot(x = x, y = y)
