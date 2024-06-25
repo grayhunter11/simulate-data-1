d2a536a738172d32ecc78a7d3fe4d42fa1ee34e0
grayhunter11 <mgrayhunter@arizona.edu>
2024-06-25 16:19:44 -0700

    Change coefficient to 3

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -6,6 +6,6 @@
 # Simulate predictor variable 
 x <- rnorm(n=100)
 # Simulate response variable with some noise
-y <- 2*x + rnorm(n=100,sd=0.2)
+y <- 3*x + rnorm(n=100,sd=0.2)
 # Plot the data
 plot(x = x, y = y)
